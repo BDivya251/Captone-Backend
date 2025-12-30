2af4046d718d3a577c48cf106e683cf80d0c1d13
Divya1-b <mail2divya.bathini@gmail.com>
2025-12-30 17:39:16 +0530

    Updated Service-regsitry

--- service-registry-service/src/main/java/com/vehiclemanagement/servicemanagement/controller/ServiceBayController.java
+++ service-registry-service/src/main/java/com/vehiclemanagement/servicemanagement/controller/ServiceBayController.java
@@ -1,7 +1,14 @@
 package com.vehiclemanagement.servicemanagement.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +20,11 @@ import com.vehiclemanagement.servicemanagement.service.ServiceBayService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/vehicle/service-bays")
 public class ServiceBayController {
 	private final ServiceBayService serviceBayService;
@@ -24,4 +33,29 @@ public class ServiceBayController {
 		ServiceBayResponse response=serviceBayService.createServiceBay(request);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
+	 @GetMapping
+	    public ResponseEntity<List<ServiceBayResponse>> getAllServiceBays() {
+	        List<ServiceBayResponse> bays = serviceBayService.getAllServiceBays();
+	        return ResponseEntity.ok(bays);
+	    }
+	    
+	    
+	    @GetMapping("/available")
+	    public ResponseEntity<List<ServiceBayResponse>> getAvailableServiceBays() {
+	        List<ServiceBayResponse> bays = serviceBayService.getAvailableServiceBays();
+	        return ResponseEntity.ok(bays);
+	    }
+	    
+	   
+	    @DeleteMapping("/{bayId}")
+	    public ResponseEntity<Map<String, String>> deleteServiceBay(@PathVariable Long bayId) {
+	        
+	        serviceBayService.deleteServiceBay(bayId);
+	        
+	        Map<String, String> response = new HashMap<>();
+	        response.put("message", "Service bay deleted successfully");
+	        response.put("bayId", bayId.toString());
+	        
+	        return ResponseEntity.ok(response);
+	    }
 }
--- service-registry-service/src/main/java/com/vehiclemanagement/servicemanagement/service/ServiceBayService.java
+++ service-registry-service/src/main/java/com/vehiclemanagement/servicemanagement/service/ServiceBayService.java
@@ -1,5 +1,9 @@
 package com.vehiclemanagement.servicemanagement.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +11,7 @@ import com.vehiclemanagement.servicemanagement.dto.request.CreateServiceBayReque
 import com.vehiclemanagement.servicemanagement.dto.response.ServiceBayResponse;
 import com.vehiclemanagement.servicemanagement.entity.ServiceBay;
 import com.vehiclemanagement.servicemanagement.exception.BadRequestException;
+import com.vehiclemanagement.servicemanagement.exception.ResourceNotFoundException;
 import com.vehiclemanagement.servicemanagement.repository.ServiceBayRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -42,4 +47,111 @@ public class ServiceBayService {
                 .isActive(bay.getIsActive())
                 .build();
 	}
+	
+	 public List<ServiceBayResponse> getAllServiceBays() {
+	        log.info("Fetching all service bays");
+	        
+	        List<ServiceBay> bays = serviceBayRepository.findAll();
+	        List<ServiceBayResponse> responseList = new ArrayList<>();
+	        
+	        for (ServiceBay bay : bays) {
+	            responseList.add(mapToResponse(bay));
+	        }
+	        
+	        return responseList;
+	    }
+	    
+	    
+	    public List<ServiceBayResponse> getAvailableServiceBays() {
+	        log.info("Fetching available service bays");
+	        
+	        List<ServiceBay> bays = serviceBayRepository. findByIsAvailableAndIsActive(true, true);
+	        List<ServiceBayResponse> responseList = new ArrayList<>();
+	        
+	        for (ServiceBay bay :  bays) {
+	            responseList.add(mapToResponse(bay));
+	        }
+	        
+	        log.info("Found {} available bays", responseList.size());
+	        
+	        return responseList;
+	    }
+	    
+	    
+	    public ServiceBay getBayByNumber(String bayNumber) {
+	        Optional<ServiceBay> bayOptional = serviceBayRepository.findByBayNumber(bayNumber);
+	        
+	        if (! bayOptional.isPresent()) {
+	            throw new ResourceNotFoundException("Service bay not found:  " + bayNumber);
+	        }
+	        
+	        return bayOptional.get();
+	    }
+	    
+	   
+	    public boolean isBayAvailable(String bayNumber) {
+	        ServiceBay bay = getBayByNumber(bayNumber);
+	        return bay.getIsAvailable() && bay.getIsActive();
+	    }
+	    
+	   
+	    @Transactional
+	    public void allocateBay(String bayNumber, Long serviceRequestId) {
+	        log.info("Allocating bay {} to service request {}", bayNumber, serviceRequestId);
+	        
+	        ServiceBay bay = getBayByNumber(bayNumber);
+	        
+	        if (!bay.getIsAvailable()) {
+	            throw new BadRequestException("Bay " + bayNumber + " is already in use by service request ID: " + 
+	                                         bay.getCurrentServiceRequestId());
+	        }
+	        
+	        if (!bay.getIsActive()) {
+	            throw new BadRequestException("Bay " + bayNumber + " is not active");
+	        }
+	        
+	        bay.setIsAvailable(false);
+	        bay.setCurrentServiceRequestId(serviceRequestId);
+	        
+	        serviceBayRepository.save(bay);
+	        
+	        log.info("Bay allocated successfully");
+	    }
+	    
+	    
+	    @Transactional
+	    public void releaseBay(String bayNumber) {
+	        log.info("Releasing bay:  {}", bayNumber);
+	        
+	        ServiceBay bay = getBayByNumber(bayNumber);
+	        
+	        bay.setIsAvailable(true);
+	        bay.setCurrentServiceRequestId(null);
+	        
+	        serviceBayRepository.save(bay);
+	        
+	        log.info("Bay released successfully");
+	    }
+	    
+	    
+	    @Transactional
+	    public void deleteServiceBay(Long bayId) {
+	        log.info("Deleting service bay ID: {}", bayId);
+	        
+	        Optional<ServiceBay> bayOptional = serviceBayRepository.findById(bayId);
+	        
+	        if (!bayOptional.isPresent()) {
+	            throw new ResourceNotFoundException("Service bay not found with ID: " + bayId);
+	        }
+	        
+	        ServiceBay bay = bayOptional.get();
+	        
+	        if (! bay.getIsAvailable()) {
+	            throw new BadRequestException("Cannot delete bay that is currently in use");
+	        }
+	        
+	        serviceBayRepository.deleteById(bayId);
+	        
+	        log.info("Service bay deleted successfully");
+	    }
 }
