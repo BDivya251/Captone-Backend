3b52b33048a19224a158dc91fd9f49275977f0c0
Divya1-b <mail2divya.bathini@gmail.com>
2025-12-31 12:27:26 +0530

    updated

--- api-gateway/src/test/java/com/vehiclemanagement/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/vehiclemanagement/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.vehiclemanagement.apigateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.vehiclemanagement.apigateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
